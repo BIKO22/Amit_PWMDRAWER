eb0cdbc88de9d69ec06de4a32bad54eb37a45caf
Mark Nady <92520896+BIKO22@users.noreply.github.com>
2022-03-11 20:39:01 +0200

    Add files via upload

---
+++ Debug/main.d
@@ -0,0 +1,121 @@
+main.d main.o: .././main.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././MCAL/DIO/DIO.h .././MCAL/DIO/../../STD_Types.h \
+ .././MCAL/DIO/../../bit_math.h .././MCAL/DIO/DIOTYPES.h \
+ .././MCAL/DIO/DIO_config.h .././MCAL/DIO/DIO.h .././bit_math.h \
+ .././MCAL/ADC/ADC.h .././MCAL/ADC/../../STD_Types.h \
+ .././MCAL/ADC/../../Bit_Math.h .././MCAL/ADC/ADC_Config.h \
+ .././MCAL/ADC/ADC.h .././MCAL/external_interrupt/extint.h \
+ .././MCAL/external_interrupt/../DIO/DIO.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ .././MCAL/TIMER0/TIMER0.h .././MCAL/TIMER0/TIMER0_Config.h \
+ .././MCAL/TIMER0/TIMER0.h .././MCAL/TIMER0/../../bit_Math.h \
+ .././HAL/LCD/LCD.h .././HAL/LCD/../../MCAL/DIO/DIO.h \
+ .././HAL/LCD/../../bit_math.h .././STD_Types.h \
+ .././MCAL/TIMER_1/TIMER_1.h .././MCAL/TIMER_1/../DIO/DIO.h \
+ .././MCAL/TIMER_1/../../bit_math.h \
+ .././MCAL/TIMER_1/../external_interrupt/extint.h \
+ .././MCAL/TIMER_1/../../STD_Types.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././MCAL/DIO/DIO.h:
+
+.././MCAL/DIO/../../STD_Types.h:
+
+.././MCAL/DIO/../../bit_math.h:
+
+.././MCAL/DIO/DIOTYPES.h:
+
+.././MCAL/DIO/DIO_config.h:
+
+.././MCAL/DIO/DIO.h:
+
+.././bit_math.h:
+
+.././MCAL/ADC/ADC.h:
+
+.././MCAL/ADC/../../STD_Types.h:
+
+.././MCAL/ADC/../../Bit_Math.h:
+
+.././MCAL/ADC/ADC_Config.h:
+
+.././MCAL/ADC/ADC.h:
+
+.././MCAL/external_interrupt/extint.h:
+
+.././MCAL/external_interrupt/../DIO/DIO.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+.././MCAL/TIMER0/TIMER0.h:
+
+.././MCAL/TIMER0/TIMER0_Config.h:
+
+.././MCAL/TIMER0/TIMER0.h:
+
+.././MCAL/TIMER0/../../bit_Math.h:
+
+.././HAL/LCD/LCD.h:
+
+.././HAL/LCD/../../MCAL/DIO/DIO.h:
+
+.././HAL/LCD/../../bit_math.h:
+
+.././STD_Types.h:
+
+.././MCAL/TIMER_1/TIMER_1.h:
+
+.././MCAL/TIMER_1/../DIO/DIO.h:
+
+.././MCAL/TIMER_1/../../bit_math.h:
+
+.././MCAL/TIMER_1/../external_interrupt/extint.h:
+
+.././MCAL/TIMER_1/../../STD_Types.h:
---
+++ Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\GLCD\GLCD.c
+
+HAL\LCD\LCD.c
+
+main.c
+
+MCAL\ADC\ADC.c
+
+MCAL\DIO\DIO.c
+
+MCAL\DIO\DIO_config.c
+
+MCAL\external_interrupt\extint.c
+
+MCAL\TIMER0\TIMER0.c
+
+MCAL\TIMER_1\TIMER_1.c
+
