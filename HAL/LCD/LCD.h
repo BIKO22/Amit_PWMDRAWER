26c848ffc16928a4dadb10ed6996eda45664cef0
Mark Nady <92520896+BIKO22@users.noreply.github.com>
2022-03-11 20:28:30 +0200

    Add files via upload

---
+++ HAL/LCD/LCD.h
@@ -0,0 +1,12 @@
+
+#ifndef HAL_LCD_LCD_H_
+#define HAL_LCD_LCD_H_
+#include"../../MCAL/DIO/DIO.h"
+#include"../../bit_math.h"
+#include<util/delay.h>
+
+void lcd_init(void);
+void lcd_sendchar(unsigned char data);
+void lcd_sendcmd(unsigned char cmd);
+void lcd_sendstring(unsigned char* string);
+#endif /* HAL_LCD_LCD_H_ */
