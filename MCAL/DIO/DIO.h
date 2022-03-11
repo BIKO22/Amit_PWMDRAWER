b465e6da17441ab211a0f02c01a1c79877afff87
Mark Nady <92520896+BIKO22@users.noreply.github.com>
2022-03-11 20:33:34 +0200

    Add files via upload

---
+++ MCAL/DIO/DIO.h
@@ -0,0 +1,16 @@
+
+#ifndef MCAL_DIO_DIO_H_
+#define MCAL_DIO_DIO_H_
+#include "../../STD_Types.h"
+#include "../../bit_math.h"
+#include"DIOTYPES.h"
+#include <avr/io.h>
+#include"DIO_config.h"
+#include <util/delay.h>
+
+
+void DIO_write(uint8 channel,uint8 state);
+int DIO_read(uint8 channel);
+void DIO_init(void);
+
+#endif /* MCAL_DIO_DIO_H_ */
