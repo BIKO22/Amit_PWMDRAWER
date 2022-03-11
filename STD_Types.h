7ed732007288d3beea11b8722c4427034b72676b
Mark Nady <92520896+BIKO22@users.noreply.github.com>
2022-03-11 20:19:35 +0200

    Add files via upload

---
+++ STD_Types.h
@@ -0,0 +1,16 @@
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+#define Enable 1
+#define Disable 0
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long int uint32;
+
+typedef signed char sint8;
+typedef signed short int sint16;
+typedef signed long int sint32;
+
+
+#endif /* STD_TYPES_H_ */
